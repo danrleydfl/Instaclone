5d91b7225a59a3bcd9937075cdab9e58e3cc3d0f
danrleyflopesoficial <danrleyflopesoficial@gmail.com>
2019-06-15 11:14:26 -0300

    Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-require('dotenv').config({ path: `${__dirname}/../.env` });
+require('dotenv').config({ path: `${__dirname}/../.env` })
 
 mongoose.connect(process.env.MONGODB_HOST, {
-  useNewUrlParser: true,
-});
+    useNewUrlParser: true,
+})
 
 app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
-app.use(cors());
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
-app.use(require('./routes'));
+    req.io = io
+    next()
+})
+app.use(cors())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+app.use(require('./routes'))
 
-server.listen(process.env.PORT, process.env.HOST);
+server.listen(process.env.PORT, process.env.HOST)
